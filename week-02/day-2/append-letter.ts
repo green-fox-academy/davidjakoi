0e79ac4f19bb8048249b8e1cf08fadedad4d155a
David Jakoi <jakoidavid92@gmail.com>
2018-08-07 15:23:44 +0200

    append-letter exercise done

---
+++ week-02/day-2/append-letter.ts
@@ -0,0 +1,19 @@
+'use strict';
+// Create a function called "appendA()" that adds "a" to every string in the far list.
+// The parameter should be a list.
+let far: string[] = ['bo', 'anacond', 'koal', 'pand', 'zebr'];
+
+function appendA(list){
+    let add: string = "a";
+    let newList = list.map(function(element){
+        return element.concat(add);
+    });
+
+    return newList;
+
+}
+
+console.log(appendA(far));
+
+// The output should be: 'boa', 'anaconda', 'koala', 'panda', 'zebra'
+export = appendA;
